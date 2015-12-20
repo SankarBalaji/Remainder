2880362221d960d53f87d883cbfc21078624e4d4
SankarBalaji <sankarmayil@gmail.com>
2015-12-20 22:31:53 +0530

    Consolidating Contacts Part-1

--- app/src/main/java/Utils/MyContacts.java
+++ app/src/main/java/Utils/MyContacts.java
@@ -1,5 +1,14 @@
 package Utils;
 
+import android.provider.ContactsContract;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by root on 17/12/15.
  */
@@ -8,6 +17,7 @@ public class MyContacts {
     final String name;
     final String photoUri;
     final long photoId;
+    final Map<Integer, Set<String>> phoneNumbers = new HashMap<>();
 
     public MyContacts (long id, String name, String photoUri, long photoId){
         this.id = id;
@@ -15,6 +25,20 @@ public class MyContacts {
         this.photoUri = photoUri;
         this.photoId = photoId;
 
+    }
+
+    public void addNumber (int type, String number) {
+        if (null == phoneNumbers.get(type))
+            phoneNumbers.put(type, new HashSet<String>());
+        phoneNumbers.get(type).add(number);
+    }
+
+    public Map<Integer, Set<String>> getAllPhonenumbers (){
+        Map<Integer, Set<String>> toReturn = new HashMap<>();
+        for (int key : phoneNumbers.keySet()) {
+            toReturn.put(key, phoneNumbers.get(key));
+        }
+        return toReturn;
     }
 
     public long getId() {
@@ -31,6 +55,22 @@ public class MyContacts {
 
     public long getPhotoId() {
         return photoId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MyContacts that = (MyContacts) o;
+
+        return name.equals(that.name);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 }
 
