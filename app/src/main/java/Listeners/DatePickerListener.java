0d8f466fddeb20ce9fe897da66ff2647214dedcf
SankarBalaji <sankarmayil@gmail.com>
2015-12-31 18:51:04 +0530

    DateChanges

--- app/src/main/java/Listeners/DatePickerListener.java
+++ app/src/main/java/Listeners/DatePickerListener.java
@@ -18,7 +18,51 @@ class DatePickerListener implements DatePickerDialog.OnDateSetListener {
 
     @Override
     public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
-        String date = dayOfMonth +"/"+monthOfYear+"/"+year;
+        String day = "";
+        if (dayOfMonth < 10)
+            day = "0"+dayOfMonth;
+        String month = "";
+        switch (monthOfYear){
+            case 0:
+                month = "January";
+                break;
+            case 1:
+                month = "February";
+                break;
+            case 2:
+                month = "March";
+                break;
+            case 3:
+                month = "April";
+                break;
+            case 4:
+                month = "May";
+                break;
+            case 5:
+                month = "June";
+                break;
+            case 6:
+                month = "July";
+                break;
+            case 7:
+                month = "August";
+                break;
+            case 8:
+                month = "September";
+                break;
+            case 9:
+                month = "October";
+                break;
+            case 10:
+                month = "November";
+                break;
+            case 11:
+                month = "December";
+                break;
+
+        }
+
+        String date = day +"-"+month+"-"+year;
         if (viewToSet instanceof EditText)
             ((EditText)viewToSet).setText(date);
     }
